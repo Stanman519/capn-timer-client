894fc26b05296da5e1be162c7798a7195f8e37e8
RyanStanley <Ryan.Stanley@dcsg.com>
2021-03-03 13:47:27 -0500

    added timers for API calls

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using RestEase;
 
 namespace ScheduledCallerClient
 {
@@ -6,7 +9,49 @@ namespace ScheduledCallerClient
     {
         static void Main(string[] args)
         {
+            ICapnAPI capnApi =
+                RestClient.For<ICapnAPI>("https://mfl-capn.herokuapp.com/Mfl/");
+            IGMBotAPI gmApi =
+                RestClient.For<IGMBotAPI>("https://capn-crunch-gm-bot.herokuapp.com/Bot/");
             var time = DateTime.Now;
+            
+            var gmBotWatch = new Stopwatch();
+            var transactionWatch = new Stopwatch();
+            
+            gmBotWatch.Start();
+            transactionWatch.Start();
+
+            while (true)
+            {
+                if (gmBotWatch.Elapsed.Days > 0)
+                {
+                    // CheckForTrades(gmApi);
+                    try
+                    {
+                        var res = gmApi.GetPendingTrades().Result;
+                        Console.WriteLine("called for trades");
+                        gmBotWatch.Restart();
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine(e.StatusCode);
+                    }
+                }
+
+                if (transactionWatch.Elapsed.Minutes > 19)
+                {
+                    try
+                    {
+                        var res = capnApi.GetTransactions().Result;
+                        Console.WriteLine("called for transactions");
+                        transactionWatch.Restart();
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine(e.StatusCode);
+                    }
+                }
+            }
             
         }
     }
