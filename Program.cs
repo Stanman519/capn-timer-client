58756cbd8e0434adc2798f17eb2080f14103622e
RyanStanley <Ryan.Stanley@dcsg.com>
2021-03-03 09:00:08 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ScheduledCallerClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = DateTime.Now;
+            
+        }
+    }
+}
